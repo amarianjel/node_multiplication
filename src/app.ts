da52b527939c2fabde243e97c5a6d9955b65203c
amarianjel <abrm.enginner@gmail.com>
2023-11-26 15:43:03 -0300

    Impresión de consola y archivo

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,28 @@
-const message: string = 'Hola';
+import fs from 'fs';
 
-console.log( message );
+//TODO: Grabar archivo de salida
+
+let outputMessage = '';
+const base = 5;
+const headerMessage = `
+========================================
+        Tabla del ${ base }
+========================================
+
+`;
+for( let i = 1; i <= 10; i++ ){
+    outputMessage+= `${ base } x ${ i } = ${ base * i }\n`;
+}
+
+outputMessage = headerMessage + outputMessage;
+
+console.log( outputMessage );
+
+//TODO: path: outputs/tabla-5.txt
+
+const outputPath = `outputs`;
+
+
+fs.mkdirSync(outputPath, { recursive: true });
+fs.writeFileSync(`${ outputPath }/tabla-${ base }.txt`, outputMessage);
+console.log('File created!');
